4dfcd52be6a3898243a845ac6cb1df4917738440
Jacob Delgado <128998284+Jdelg140@users.noreply.github.com>
2025-11-22 02:58:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import json
 import requests
 from gtts import gTTS
-from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip, vfx
+from moviepy.editor import (
+VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip, vfx
+)
 import google.generativeai as genai
 from PIL import Image
 
